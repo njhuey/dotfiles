79049683ff9816e4dafe16968868e962b1da7842
Nathan Huey <nate.huey4@gmail.com>
2024-06-11 16:30:25 -0700

    feat: add tmux.conf

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 # Install dotfiles.
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.config/tmux/tmux.conf
 
 # Install nvim.
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
