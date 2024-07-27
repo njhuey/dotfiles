4563165cab3f051c14eb605e97fc932541e77efa
Nathan Huey <nate.huey4@gmail.com>
2024-07-27 19:10:07 -0400

    feat: add kitty config file

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 ln -s $HOME/.dotfiles/gitconfig $HOME/.config/git/config
 ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/tmux.conf $HOME/.config/tmux/tmux.conf
+ln -s $HOME/.dotfiles/kitty.conf $HOME/.config/kitty/kitty.conf
 
 # Install nvim.
 ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
